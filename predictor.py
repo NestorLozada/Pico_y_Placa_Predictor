663d1f51f3844354e2648fd46f5b213ae1fc4a40
NestoLozada <martinlozada1@gmail.com>
2023-02-12 10:05:10 -0500

    correcting messages

--- predictor.py
+++ predictor.py
@@ -8,16 +8,19 @@ def pico_y_placa(plate_number, date, time):
 
     # Check if the car can be on the road
     if day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
-        if 7 <= int(time[:2]) <= 9:
-            return last_digit in [1, 2]
-        elif 16 <= int(time[:2]) <= 19:
-            return last_digit in [3, 4]
+        if 7 <= int(time[:2]) <= 9 or 16 <= int(time[:2]) <= 19:
+            if last_digit == 1 or last_digit == 2:
+                return "no puede circular"
+            elif last_digit == 3 or last_digit == 4:
+                return "no puede circular"
+            else:
+                return "puede circular"
         else:
-            return True
+            return "puede circular"
     else:
-        return True
+        return "puede circular"
 
 # Example usage
-print(pico_y_placa("PBX-1234", "2023-02-10", "08:30"))  # Output: False
-print(pico_y_placa("PBX-1234", "2023-02-11", "17:30"))  # Output: False
-print(pico_y_placa("PBX-1234", "2023-02-12", "07:00"))  # Output: True
+print(pico_y_placa("PBX-1234", "2023-02-10", "08:30"))  # Output: no puede circular
+print(pico_y_placa("PBX-1234", "2023-02-11", "17:30"))  # Output: no puede circular
+print(pico_y_placa("PBX-1234", "2023-02-12", "07:00"))  # Output: puede circular
